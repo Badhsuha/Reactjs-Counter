df15ac8d35ad18a8f8fba6eae1abdb0c9d0b59f3
Badhusha Muhammed <59449374+Badhsuha@users.noreply.github.com>
2021-04-29 22:42:58 +0530

    alignment chaged in counters.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,27 +18,37 @@ class Counter extends Component {
 
     return (
       <React.Fragment>
-        <span className={setClassName()}>{setCounter()}</span>
-        <button
-          className="btn btn-secondory btn-sm m-2"
-          onClick={() => this.props.onDecriment(this.props.counter)}
-          disabled={setBtnDisabled()}
+        <div
+          className="d-flex justify-content-between"
+          style={{ width: 200, height: 50 }}
         >
-          Decriment
-        </button>
-        <button
-          className="btn btn-secondory btn-sm"
-          onClick={() => this.props.onIncriment(this.props.counter)}
-        >
-          Incriment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter)}
-        >
-          Delete
-        </button>
-        <br></br>
+          <span
+            className={setClassName()}
+            style={{ paddingTop: 10, width: 30 }}
+          >
+            {setCounter()}{" "}
+          </span>
+          <button
+            className="btn btn-secondory btn-sm m-2"
+            onClick={() => this.props.onDecriment(this.props.counter)}
+            disabled={setBtnDisabled()}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-secondory btn-sm m-2"
+            onClick={() => this.props.onIncriment(this.props.counter)}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.counter)}
+          >
+            X
+          </button>
+          <br></br>
+        </div>
       </React.Fragment>
     );
   }
